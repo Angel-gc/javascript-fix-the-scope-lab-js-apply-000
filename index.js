7e8e704836abbad7911afc17e3f959a2c54ea371
Angel Concepcion <angelconcepcion50@gmail.com>
2020-03-10 21:03:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ function add2(n) {
   // const two = 2
 }
 
-var funkyFunction = function(theFunk()) {
-  return function theFunk() {
+var theFunk = function() {
+  return function funkyFunction() {
     return "FUNKY!"
   }
 }
