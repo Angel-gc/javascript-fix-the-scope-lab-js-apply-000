c880ff7713119d67dbece058dda06b02b31ea7de
Angel Concepcion <angelconcepcion50@gmail.com>
2020-03-10 21:05:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,11 +24,14 @@ function add2(n) {
   // const two = 2
 }
 
-var theFunk = function() {
-  return function theFunk() {
-    return "FUNKY!"
+  var funkyFunction = function() {
+    return function(){
+      return "FUNKY!"
+    }
   }
-}
+
+  var theFunk = funkyFunction();
+  
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
